994b8f5e3e7f9778d72bc5cea7218e9db8776ce8
AnnaWastling <45712410+AnnaWastling@users.noreply.github.com>
2018-12-08 13:10:52 +0100

    Add files via upload

---
+++ Test-tabell.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+ </head>
+<body>
+
+<table> 
+<tr>
+<th> ItemID </th> <!-- <th>Picture</th> <th>Color</th> <th>Part name</th> -->
+</tr>
+
+<?php $connection = mysqli_connect("mysql.itn.liu.se","lego","","lego"); 
+	if (!$connection) 
+	{
+		die('MySQL connection error');
+	} 
+	
+	$contents = mysqli_query($connection,"SELECT COUNT(DISTINCT ItemID, ColorID) AS NumberOfLego FROM inventory ");
+	
+	for ($x = 1; $x <= $contents->num_rows; $x++) 
+{
+	 $row = mysqli_fetch_array($contents);
+	
+	
+	echo "<tr>", 
+		 "<td>", $row [NumberOfLego], "</td>", 
+		 "</tr>";
+}
+
+mysqli_close($connection); 
+
+?>
+
+</table>
+
+</body>
+</html>
